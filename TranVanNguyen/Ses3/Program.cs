7b129ed4e30969d281e53fce69391c1d9984da89
Tran Van Nguyen <43206354+giazzz@users.noreply.github.com>
2019-05-27 23:30:02 +0700

    tiet 2

---
+++ TranVanNguyen/Ses3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ses3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt16(Console.ReadLine());
+            string msg = "";
+            if(num < 0)
+            {
+                msg = "The number "+num+" is negative";
+            }
+            else if(num %2 == 0)
+            {
+                msg = "The number "+num+" is even";
+            }
+            else
+            {
+                msg = "The number "+num+" is odd";
+            }
+            if(msg != "")
+                Console.WriteLine(msg);
+        }
+    }
+}
