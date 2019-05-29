2608314a8ac5ad9d0fa0caa3ebd2f367609e6b5d
Duy Khanh <giovotinh15758@gmail.com>
2019-05-29 11:54:22 +0700

    create new project

---
+++ PhamDuyKhanh/project1/project1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Nhap 1 so: ");
+            //int num = Convert.ToInt16(Console.ReadLine());
+            //String msg = "";
+            //if (num < 0)
+            //{
+            //    msg = num + "la so nguyen";
+            //}
+            //else if (num % 2 == 0)
+            //{
+            //    msg = num + "la so chan";
+            //}
+            //else
+            //{
+            //    msg = num + "la so le";
+            //}
+            //Console.WriteLine(msg);
+            //Console.ReadLine();
+
+            //Nhap vao 1 so nguyen duong tu 1 - 5, hien thi tat ca
+            //    so chan co so chu so = so nguyen duong vua nhap
+            //vi du: nhap 1, hien thi : 2,4,6,8
+            // nhap 2: hien thi: 10.12.14.56.88.99
+
+            Console.WriteLine("Nhap vao 1 chu so: ");
+            int num = Convert.ToInt16(Console.ReadLine());
+
+            double start = Math.Pow(10,num -1);
+            double end = Math.Pow(10, num) -1;
+
+            for (double i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("{0} la so chan", i);
+                }
+                else
+                {
+                    //Console.WriteLine("{0} la so chan", i);
+                }
+            }
+            //dung while
+            double j = start;
+            while (j <= end);
+            {
+                j++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PhamDuyKhanh/project2/project2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao so thu 1:");
+            int num1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Nhap vao so thu 2:");
+            int num2  = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("=========888=========");
+
+            Tong(num1, num2);
+            Hieu(num1, num2);
+            Tich(num1, num2);
+            Thuong(num1, num2);
+            Console.ReadLine();
+        }
+        public static void Tong(int a, int b)
+        {
+            Console.WriteLine("Tong 2 so la: " + (a + b));   
+        }
+
+        public static void Hieu(int a, int b)
+        {
+            Console.WriteLine("Hieu 2 so la: " + (a - b));
+        }
+
+        public static void Tich(int a, int b)
+        {
+            Console.WriteLine("Tich 2 so la: " + (a * b));
+        }
+        public static void Thuong(int a, int b)
+        {
+            Console.WriteLine("Thuong 2 so la: " + (a / b));
+        }
+    }
+}
