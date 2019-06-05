f9ddee3b3a499ec79dad706a2f71f8a78af94164
rabbithunter0502 <leduc.road50@gmail.com>
2019-06-05 14:52:36 +0700

    Test

---
+++ LeTrongDuc/ConsoleApplication1/ConsoleApplication1/NhanVien.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThongTinNhanVien
+{
+    class NhanVien
+    {
+        short _MaNV;
+
+        public Int16 MaNV
+        {
+            get { return _MaNV; }
+            set { _MaNV = value; }
+        }
+        string _TenNV;
+
+        public string TenNV
+        {
+            get { return _TenNV; }
+            set { _TenNV = value; }
+        }
+
+        public NhanVien(short MaNV, string TenNV)
+        {
+            // TODO: Complete member initialization
+            this._MaNV = MaNV;
+            this._TenNV = TenNV;
+        }
+    }
+}
---
+++ LeTrongDuc/Studentdetails/Studentdetails/ScoreReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager
+{
+    class ScoreReport
+    {
+        private string _subject = "C-Sharp";
+        private int _mark = 80;
+    }
+}
