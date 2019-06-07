13ca7576a73b8919a743a71778ffd5ab15453883
rabbithunter0502 <leduc.road50@gmail.com>
2019-06-07 23:16:28 +0700

    update

---
+++ LeTrongDuc/bt9. QuanLyHoaDon/QuanLyHoaDon/QtyPrice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyHoaDon
+{
+    class QtyPrice
+    {
+        Int32 _quota;
+
+        public Int32 Quota
+        {
+            get { return _quota; }
+            set { _quota = value; }
+        }
+        float _price;
+
+        public float Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public QtyPrice() 
+        {
+
+        }
+    }
+}
